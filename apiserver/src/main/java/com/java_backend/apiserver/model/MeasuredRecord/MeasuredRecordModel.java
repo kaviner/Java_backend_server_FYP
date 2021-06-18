d3f3d26a1692942ec45ac69b7067a3418ce5c209
Vincent Chan <vincentchan0327@gmail.com>
2021-06-18 11:26:21 +0800

    Add constructor

--- apiserver/src/main/java/com/java_backend/apiserver/model/MeasuredRecord/MeasuredRecordModel.java
+++ apiserver/src/main/java/com/java_backend/apiserver/model/MeasuredRecord/MeasuredRecordModel.java
@@ -54,8 +54,16 @@ public class MeasuredRecordModel {
     private String lowest_FatigueLevel_Label;  // The lowest fatigue level label 
     private String lowest_BPM_Label;           // The lowest bpm label
 
-
-    public MeasuredRecordModel() {
+    public MeasuredRecordModel(String userID, String measureID, String deviceID, String sensorID, String activityID, String activityName, String category, Date startDateTime, Date endDateTime) {
+        this.userID = userID;
+        this.measureID = measureID;
+        this.deviceID = deviceID;
+        this.sensorID = sensorID;
+        this.activityID = activityID;
+        this.activityName = activityName;
+        this.category = category;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
     }
 
     public MeasuredRecordModel(ObjectId _id, String userID, String measureID, String deviceID, String sensorID, String activityID, String activityName, String category, Date startDateTime, Date endDateTime, double avg_RRI_Value, double avg_StressLevel_Value, double avg_FatigueLevel_Value, double avg_BPM_Value, String avg_RRI_Label, String avg_StressLevelLabel, String avg_FatigueLevel_Label, String avg_BPM_Label, double highest_RRI_Value, double highest_StressLevel_Value, double highest_FatigueLevel_Value, double highest_BPM_Value, String highest_RRI_Label, String highest_StressLevelLabel, String highest_FatigueLevel_Label, String highest_BPM_Label, double lowest_RRI_Value, double lowest_StressLevel_Value, double lowest_FatigueLevel_Value, double lowest_BPM_Value, String lowest_RRI_Label, String lowest_StressLevelLabel, String lowest_FatigueLevel_Label, String lowest_BPM_Label) {

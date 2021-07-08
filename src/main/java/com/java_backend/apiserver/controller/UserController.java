42926335f712dd03997155592dad90fb7c510b49
Vincent Chan <vincentchan0327@gmail.com>
2021-07-08 17:17:52 +0800

    Add checkUserIDExist and create function

---
+++ src/main/java/com/java_backend/apiserver/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.java_backend.apiserver.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.java_backend.apiserver.mongo_operation.UserService;
+import com.java_backend.apiserver.util.RequestVerifyUtil;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class UserController {
+    @PostMapping(value = "/checkUserIDExist",consumes = "application/json", produces = "application/json")
+    public HashMap<String,String> checkUserIDExist(@RequestBody Map<String,String> user) {
+        RequestVerifyUtil.printRequestBody("/checkUserIDExist", user.toString());
+        UserService userService = new UserService();
+        HashMap<String,String> result = userService.checkUserIDExist(user.get("_id"),user.get("alias"));
+        return result;
+    }
+}
---
+++ src/main/java/com/java_backend/apiserver/mongo_operation/UserService.java
@@ -0,0 +1,64 @@
+package com.java_backend.apiserver.mongo_operation;
+import com.java_backend.apiserver.config.mongodb_config.MongoConfig;
+import com.java_backend.apiserver.model.Activity;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.InsertOneResult;
+
+import static com.mongodb.client.model.Filters.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.json.JSONArray;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+public class UserService {
+    private ApplicationContext context;
+    private MongoTemplate mongo;
+    private String databaseName;
+    private MongoDatabase db;
+    private MongoCollection<Document> userCollection;
+    private MongoClient mongoClient;
+    private MongoTemplate mongoTemplate;
+
+    public UserService() {
+        try {
+            context = new AnnotationConfigApplicationContext(MongoConfig.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        mongoClient = context.getBean(MongoClient.class);
+        mongoTemplate = context.getBean(MongoTemplate.class);
+        databaseName = context.getBean(String.class);
+        db = mongoClient.getDatabase(databaseName);
+        userCollection = db.getCollection("User");
+    }
+
+    public HashMap<String, String> checkUserIDExist(String mobileID,String alias) {
+        Bson filter = eq("mobileId", mobileID);
+        Document user = userCollection.find(filter).first();
+        HashMap<String,String> response  = new HashMap<String,String>();
+        if(user==null){
+            Document newUserProfile = new Document();
+            newUserProfile.append("alias", alias);
+            newUserProfile.append("mobileId", mobileID);
+            InsertOneResult result= userCollection.insertOne(newUserProfile);
+            response.put("result", result.getInsertedId().asString().toString());
+        }else{
+            response.put("result", user.get("_id").toString());
+        }
+        return response;
+    }
+
+}
